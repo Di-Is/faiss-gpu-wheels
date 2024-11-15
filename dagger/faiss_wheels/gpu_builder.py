96a5747a68ee161d59681982a259d0b41ef62059
Di-Is <rhoxbox@gmail.com>
2024-11-16 08:21:38 +0900

    refactor: simplify wheel name handling (#42)

--- dagger/faiss_wheels/gpu_builder.py
+++ dagger/faiss_wheels/gpu_builder.py
@@ -6,8 +6,6 @@ This software is released under the MIT License.
 http://opensource.org/licenses/mit-license.php
 """
 
-from pathlib import Path
-
 from dagger import BuildArg, Container, Directory, File
 
 from .builder import AbsWheelBuilder
@@ -15,7 +13,7 @@ from .type import AuditWheelConfig, BuildConfig, CUDAConfig, PythonConfig
 
 
 class ImageBuilder:
-    """image builder for faiss-gpu."""
+    """image ctr for faiss-gpu."""
 
     def __init__(
         self,
@@ -37,14 +35,14 @@ class ImageBuilder:
         self._auditwheel_config = auditwheel_config
         self._cuda_config = cuda_config
 
-    async def build(self) -> Container:
+    def build(self) -> Container:
         """Build image using faiss-gpu wheel building.
 
         Returns:
             container for faiss-gpu wheel building
         """
-        # build wheel builder image
-        builder = self._host_directory.docker_build(
+        # build wheel ctr image
+        return self._host_directory.docker_build(
             dockerfile="./docker/Dockerfile.gpu",
             build_args=[
                 BuildArg("CUDA_MAJOR_VERSION", self._cuda_config["major_version"]),
@@ -55,19 +53,14 @@ class ImageBuilder:
                 BuildArg("AUDITWHEEL_POLICY", self._auditwheel_config["policy"]),
             ],
         )
-        await builder.sync()
-        return builder
 
 
 class WheelBuilder(AbsWheelBuilder):
-    """faiss-gpu wheel builder."""
-
-    _raw_dir: str = "/output/raw_wheels"
-    _repaired_dir: str = "/output/repaired_wheels"
+    """faiss-gpu wheel ctr."""
 
     def __init__(  # noqa: PLR0913
         self,
-        builder: Container,
+        ctr: Container,
         host_directory: Directory,
         build_config: BuildConfig,
         auditwheel_config: AuditWheelConfig,
@@ -77,14 +70,14 @@ class WheelBuilder(AbsWheelBuilder):
         """constructor.
 
         Args:
-            builder: container for faiss-gpu wheel building
+            ctr: container for faiss-gpu wheel building
             host_directory: repository root directory
             build_config: build config
             auditwheel_config: auditwheel config
             python_config: python config
             cuda_config: cuda config
         """
-        self._builder = builder
+        self._ctr = ctr
         self._host_directory = host_directory
         self._build_config = build_config
         self._auditwheel_config = auditwheel_config
@@ -100,8 +93,8 @@ class WheelBuilder(AbsWheelBuilder):
         Returns:
             faiss-gpu wheel file
         """
-        builder = (
-            self._builder.with_directory("/project", self._host_directory)
+        ctr = (
+            self._ctr.with_directory("/project", self._host_directory)
             .with_workdir("/project")
             .with_env_variable("CUDA_MAJOR_VERSION", self._cuda_config["major_version"])
             .with_env_variable("CUDA_MINOR_VERSION", self._cuda_config["minor_version"])
@@ -111,119 +104,20 @@ class WheelBuilder(AbsWheelBuilder):
                 "CUDA_DYNAMIC_LINK", "ON" if self._cuda_config["dynamic_link"] else ""
             )
             .with_env_variable(
-                "PYTHON_SUPPORT_VERSIONS",
-                ";".join(self._python_config["support_versions"]),
+                "PYTHON_SUPPORT_VERSIONS", ";".join(self._python_config["support_versions"])
             )
             .with_env_variable("FAISS_OPT_LEVEL", self._build_config["instruction_set"])
             .with_env_variable("FAISS_ENABLE_GPU", "ON")
             .with_env_variable("FAISS_ENABLE_RAFT", "OFF")
-            .with_exec(["mkdir", "-p", self._raw_dir, self._repaired_dir])
+            .with_env_variable("UV_HTTP_TIMEOUT", "10000000")
+            .with_exec(["uv", "build", "--wheel", "--python", python_target_version])
         )
 
-        builder = builder.with_exec(
-            [
-                "uv",
-                "build",
-                "--wheel",
-                "--python",
-                f"python{python_target_version}",
-                "--out-dir",
-                self._raw_dir,
-            ]
-        )
-
-        # get faiss version
-        faiss_ver = await self._host_directory.file("version.txt").contents()
-        faiss_ver = faiss_ver.replace("\n", "")
-
-        whlname_maker = WheelName(
-            faiss_ver,
-            self._auditwheel_config["policy"],
-            self._cuda_config["major_version"],
-        )
-
-        builder = builder.with_exec(
-            [
-                "auditwheel",
-                "repair",
-                "-w",
-                self._repaired_dir,
-                str(Path(self._raw_dir) / whlname_maker.make_raw_wheelname(python_target_version)),
-            ]
+        raw_wheel_name = (await ctr.directory("dist").glob("*.whl"))[0]
+        ctr = ctr.with_exec(
+            ["auditwheel", "repair", f"dist/{raw_wheel_name}"]
             + self._auditwheel_config["repair_option"]
         )
-        await builder.sync()
 
-        return builder.directory(self._repaired_dir).file(
-            whlname_maker.make_repaired_wheelname(python_target_version)
-        )
-
-
-class WheelName:
-    """faiss-gpu wheel name provider."""
-
-    def __init__(
-        self, faiss_version: str, auditwheel_policy: str, cuda_major_version: str
-    ) -> None:
-        """constructor.
-
-        Args:
-            faiss_version: faiss python package version
-            auditwheel_policy: auditwheel policy
-            cuda_major_version: cuda major version
-        """
-        self._faiss_version = faiss_version
-        self._auditwheel_policy = auditwheel_policy
-        self._cuda_major_version = cuda_major_version
-        self._arch = "x86_64"
-        self._platform = "linux"
-
-    def make_raw_wheelname(self, python_version: str) -> str:
-        """Make raw wheel name.
-
-        Args:
-            python_version: python version {major}.{minor}
-
-        Returns:
-            raw wheel name
-        """
-        python_version = "cp" + python_version.replace(".", "")
-        name_parts = [
-            f"faiss_gpu_cu{self._cuda_major_version}",
-            self._faiss_version,
-            python_version,
-            python_version,
-            f"{self._platform}_{self._arch}",
-        ]
-        return "-".join(name_parts) + ".whl"
-
-    def make_repaired_wheelname(self, python_version: str) -> str:
-        """Make repaired wheel name.
-
-        Args:
-            python_version: python version {major}.{minor}
-
-        Returns:
-            repaired wheel name
-        """
-        python_version = "cp" + python_version.replace(".", "")
-        if self._auditwheel_policy == "manylinux2014":
-            name_parts = [
-                f"faiss_gpu_cu{self._cuda_major_version}",
-                self._faiss_version,
-                python_version,
-                python_version,
-                f"manylinux_2_17_{self._arch}.{self._auditwheel_policy}_{self._arch}",
-            ]
-        elif self._auditwheel_policy == "manylinux_2_28":
-            name_parts = [
-                f"faiss_gpu_cu{self._cuda_major_version}",
-                self._faiss_version,
-                python_version,
-                python_version,
-                f"{self._auditwheel_policy}_{self._arch}",
-            ]
-        else:
-            msg = f"Invalid auditwheel policy. {self._auditwheel_policy}"
-            raise ValueError(msg)
-        return "-".join(name_parts) + ".whl"
+        repaired_wheel_name = (await ctr.directory("wheelhouse").glob("*.whl"))[0]
+        return ctr.directory("wheelhouse").file(repaired_wheel_name)
