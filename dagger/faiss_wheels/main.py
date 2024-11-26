ddc24f2f03145076c178fb549fb7ad0b3febe2f7
Di-Is <rhoxbox@gmail.com>
2024-11-26 23:37:02 +0900

    change: re-introduce cibuildwheel

--- dagger/faiss_wheels/main.py
+++ dagger/faiss_wheels/main.py
@@ -11,12 +11,9 @@ from __future__ import annotations
 from typing import Annotated
 
 import dagger
-from dagger import dag, function, object_type
-from faiss_wheels import _cpu_builder, _gpu_cuda_builder
+from dagger import function, object_type
 
 from ._static_analysis import static_analysis
-from ._test import execute_test
-from ._util import execute_async_parallel, get_compatible_python, load_config
 
 
 @object_type
@@ -33,76 +30,3 @@ class FaissWheels:
             source: source directory
         """
         await static_analysis(self.source)
-
-    @function
-    async def build_container(self, config_file: dagger.File) -> dagger.Container:
-        """Build image included faiss library.
-
-        Args:
-            config_file: config file.
-
-        Returns:
-            container for faiss building
-        """
-        config = await load_config(config_file)
-        match config.variant:
-            case "cpu":
-                builder = _cpu_builder.ImageBuilder(self.source, config)
-            case "gpu-cu11" | "gpu-cu12":
-                builder = _gpu_cuda_builder.ImageBuilder(self.source, config)
-            case _:
-                raise NotImplementedError
-        ctr = builder.build()
-        return ctr.with_directory("/project", self.source).with_workdir("/project")
-
-    @function
-    async def build_wheels(self, config_file: dagger.File) -> dagger.Directory:
-        """Build faiss wheels.
-
-        Args:
-            config_file: config file.
-
-        Returns:
-            container for faiss building
-        """
-        ctr = await self.build_container(config_file)
-        config = await load_config(config_file)
-        match config.variant:
-            case "cpu":
-                builder = _cpu_builder.WheelBuilder(ctr, config)
-            case "gpu-cu11" | "gpu-cu12":
-                builder = _gpu_cuda_builder.WheelBuilder(ctr, config)
-            case _:
-                raise NotImplementedError
-        wheels = await execute_async_parallel(
-            builder.build,
-            [{"py_version": v} for v in get_compatible_python(config.python.requires_python)],
-            config.python.njob,
-        )
-        return dag.directory().with_files(".", wheels)
-
-    @function
-    async def execute_pipeline(self, config_file: dagger.File) -> dagger.Directory:
-        """Execute pipeline.
-
-        Args:
-            config_file: config file.
-
-        Returns:
-            directory included wheels
-        """
-        await self.static_analysis()
-        wheel_dir = await self.build_wheels(config_file)
-        await self.execute_test(wheel_dir, config_file)
-        return wheel_dir
-
-    @function
-    async def execute_test(self, wheel_dir: dagger.Directory, config_file: dagger.File) -> None:
-        """Execute test.
-
-        Args:
-            wheel_dir: directory included wheels
-            config_file: config file
-        """
-        config = await load_config(config_file)
-        await execute_test(self.source, wheel_dir, config)
---
+++ script/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Enable yum cache
+YUM_CACHE_DIR='/host/tmp/.cache/yum'
+cat <<EOF > /etc/yum.conf
+[main]
+cachedir=${YUM_CACHE_DIR}
+keepcache=1
+EOF
+[ ! -d "$YUM_CACHE_DIR" ] && mkdir -p "$YUM_CACHE_DIR" && yum makecache
+
+# Install blas
+yum install -y openblas-devel openblas-static
+
+FAISS_ENABLE_GPU=${FAISS_ENABLE_GPU:-"OFF"}
+FAISS_ENABLE_ROCM=${FAISS_ENABLE_ROCM:-"OFF"}
+FAISS_ENABLE_RAFT=${FAISS_ENABLE_RAFT:-"OFF"}
+if [ "$FAISS_ENABLE_GPU" == "ON" ]; then
+    if [ "$FAISS_ENABLE_ROCM" != "ON" ]; then
+        CUDA_MAJOR_VERSION=$(echo "$CUDA_VERSION" | cut -d '.' -f 1)
+        CUDA_MINOR_VERSION=$(echo "$CUDA_VERSION" | cut -d '.' -f 2)
+        # Install CUDA
+        yum -y install yum-utils
+        yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+        yum -y install cuda-toolkit-${CUDA_MAJOR_VERSION}-${CUDA_MINOR_VERSION}-${CUDA_VERSION}
+        ln -s cuda-${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION} /usr/local/cuda
+        export PATH="/usr/local/cuda/bin:${PATH}"
+    fi
+fi
+
+# Build and patch faiss
+FAISS_OPT_LEVEL=${FAISS_OPT_LEVEL:-"generic"}
+CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES:-"80-real"}
+NJOB=${NJOB:-"3"}
+cd faiss
+cmake . -B build \
+    -DFAISS_OPT_LEVEL=${FAISS_OPT_LEVEL} \
+    -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+    -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES} \
+    -DFAISS_ENABLE_RAFT=${FAISS_ENABLE_RAFT} \
+    -DFAISS_ENABLE_ROCM=${FAISS_ENABLE_ROCM} \
+    -DFAISS_ENABLE_PYTHON=OFF \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j${NJOB}
+cmake --install build
+cd ..
+git apply patch/modify-numpy-find-package.patch
