ec36f67a2844d4d725e32675fadc6eb9c7dcb36c
Di-Is <rhoxbox@gmail.com>
2025-02-01 20:32:14 +0900

    update: bump faiss version to v1.10.0 (#69)

--- script/generate_pyproject.py
+++ script/generate_pyproject.py
@@ -133,7 +133,7 @@ pytest {project}/faiss/tests/torch_test_contrib.py -n $((`nproc --all`/5+1))
 pytest {project}/faiss/tests/ -n $((`nproc --all`/5+1)) &&
 pytest {project}/faiss/tests/torch_test_contrib.py -n $((`nproc --all`/5+1)) &&
 # GPU Test
-pytest {project}/faiss/tests/common_faiss_tests.py {project}/faiss/faiss/gpu/test/ -n 4 &&
+pytest {project}/faiss/tests/common_faiss_tests.py {project}/faiss/faiss/gpu/test/ -n 2 &&
 pytest {project}/faiss/faiss/gpu/test/torch_test_contrib_gpu.py
 """
         if variant == "gpu-cu11":
