4a6995be064adb4115a5524186dd200028e076be
Di-Is <rhoxbox@gmail.com>
2025-04-27 11:41:49 +0900

    a

--- script/make_cache.sh
+++
@@ -1,9 +0,0 @@
-python_vers=(3.9 3.10 3.11 3.12)
-variants=("cpu" "gpu-cu11" "gpu-cu12")
-for var in ${python_vers[@]}; do
-    for variant in ${variants[@]}; do
-        echo "Building $variant with Python $var"
-        uv sync --python $var --no-install-project --project variant/$variant
-        rm -r variant/$variant/.venv
-    done
-done
